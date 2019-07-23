cadade45b3087c5bbbde802908312b373c15fba3
lcorcodilos <lac352@scarletmail.rutgers.edu>
2019-07-23 16:08:32 -0400

    Update condor environment

--- condor/prep.csh
+++ condor/prep.csh
@@ -2,5 +2,5 @@ cd /uscms_data/d3/lcorcodi/BStar13TeV/CMSSW_10_2_0/src/BStar13TeV/rootfiles/
 tar -czf bstar_presel_rootfiles.tgz TWpreselection1*_tau32medium*.root
 xrdcp -f bstar_presel_rootfiles.tgz root://cmseos.fnal.gov//store/user/lcorcodi/bstar_presel_rootfiles.tgz
 rm bstar_presel_rootfiles.tgz
-cd /uscms_data/d3/lcorcodi/BStar13TeV/CMSSW_8_1_0/src/2DAlphabet
+cd /uscms_data/d3/lcorcodi/BStar13TeV/CMSSW_10_2_13/src/2DAlphabet
 
--- condor/run_scripts/run_limits_bstar.sh
+++ condor/run_scripts/run_limits_bstar.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 source /cvmfs/cms.cern.ch/cmsset_default.sh
-xrdcp root://cmseos.fnal.gov//store/user/lcorcodi/2Dalpha.tgz ./
+xrdcp root://cmseos.fnal.gov//store/user/lcorcodi/10XwithNano.tgz ./
 export SCRAM_ARCH=slc6_amd64_gcc700
 scramv1 project CMSSW CMSSW_10_2_13
-tar -xzf 2Dalpha.tgz
-rm 2Dalpha.tgz
+tar -xzf 10XwithNano.tgz
+rm 10XwithNano.tgz
 
 mkdir tardir; cp tarball.tgz tardir/; cd tardir
 tar -xzf tarball.tgz
@@ -14,10 +14,12 @@ eval `scramv1 runtime -sh`
 scramv1 b clean; scramv1 b
 
 # Grab the rootfiles
-xrdcp root://cmseos.fnal.gov//store/user/lcorcodi/bstar_presel_rootfiles.tgz ./
 mkdir rootfiles
+cd rootfiles
+xrdcp root://cmseos.fnal.gov//store/user/lcorcodi/bstar_presel_rootfiles.tgz ./
 tar -xzf bstar_presel_rootfiles.tgz
 rm bstar_presel_rootfiles.tgz
+cd ../
 
 echo run_Limit.py $*
 python run_Limit.py $* 
