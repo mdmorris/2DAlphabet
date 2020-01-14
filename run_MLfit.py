1faae7c3b95461c362dc129e7403c2e4f897830e
Lucas Corcodilos <lac352@scarletmail.rutgers.edu>
2020-01-14 11:42:00 -0500

    Missed parts for 3482d34aaf544f4d27ae6cd9be7f81194da5d806

--- run_MLfit.py
+++ run_MLfit.py
@@ -61,76 +61,77 @@ for i in inputConfigs:
 twoDinstances = []
 
 # If simultaneous fit
-if len(inputConfigs) > 1:
-    # Instantiate all class instances
-    for i in inputConfigs:
-        instance = TwoDAlphabet(i,options.quicktag,options.recycleAll,stringSwaps=stringSwaps)
-        twoDinstances.append(instance)
+# if len(inputConfigs) > 1:
+##############
+# Instantiate all class instances
+for i in inputConfigs:
+    instance = TwoDAlphabet(i,options.quicktag,options.recycleAll,stringSwaps=stringSwaps)
+    twoDinstances.append(instance)
 
-    # For each instance, check tags match and if they don't, ask the user for one
+# For each instance, check tags match and if they don't, ask the user for one
+for t in twoDinstances:
+    if t.tag != twoDinstances[0].tag:
+        print 'ERROR: tags in configuration files do not match. '+t.tag+' does not match '+twoDinstances[0].tag+'. Please make sure they match and try again. Quitting...'
+        quit()
+thistag = twoDinstances[0].tag
+
+# Combine the cards
+print 'cd ' + thistag
+with header.cd(thistag):
+    card_combination_command = 'combineCards.py'
+    for i in twoDinstances:
+        card_combination_command += ' '+i.name+'/card_'+i.name+'.txt'
+    card_combination_command += ' > card_'+thistag+'.txt'
+
+    print 'Executing ' + card_combination_command
+    subprocess.call([card_combination_command],shell=True)
+    for num in range(1,len(twoDinstances)+1):
+        subprocess.call(["sed -i 's/ch"+str(num)+"_//g' card_"+thistag+".txt"],shell=True)
+
+if not options.skipFit:
+    runMLFit(twoDinstances,options.rMin,options.rMax,systsToSet=options.setParameter,skipPlots=options.skipPlots)
+
+# Plot
+if not options.skipPlots:
     for t in twoDinstances:
-        if t.tag != twoDinstances[0].tag:
-            print 'ERROR: tags in configuration files do not match. '+t.tag+' does not match '+twoDinstances[0].tag+'. Please make sure they match and try again. Quitting...'
-            quit()
-    thistag = twoDinstances[0].tag
+        try:
+            t.plotFitResults('b')
+        except Exception as exc:
+            print traceback.format_exc()
+            print exc
+            print 'Failed to run b plots for '+t.name
+        try:
+            t.plotFitResults('s')
+        except Exception as exc:
+            print traceback.format_exc()
+            print exc
+            print 'Failed to run s plots for '+t.name
 
-    # Combine the cards
-    print 'cd ' + thistag
-    with header.cd(thistag):
-        card_combination_command = 'combineCards.py'
-        for i in twoDinstances:
-            card_combination_command += ' '+i.name+'/card_'+i.name+'.txt'
-        card_combination_command += ' > card_'+thistag+'.txt'
-
-        print 'Executing ' + card_combination_command
-        subprocess.call([card_combination_command],shell=True)
-        for num in range(1,len(twoDinstances)+1):
-            subprocess.call(["sed -i 's/ch"+str(num)+"_//g' card_"+thistag+".txt"],shell=True)
-
-    if not options.skipFit:
-        runMLFit(twoDinstances,options.rMin,options.rMax,systsToSet=options.setParameter,skipPlots=options.skipPlots)
-
-    # Plot
-    if not options.skipPlots:
-        for t in twoDinstances:
-            try:
-                t.plotFitResults('b',simfit=True)
-            except Exception as exc:
-                print traceback.format_exc()
-                print exc
-                print 'Failed to run b plots for '+t.name
-            try:
-                t.plotFitResults('s',simfit=True)
-            except Exception as exc:
-                print traceback.format_exc()
-                print exc
-                print 'Failed to run s plots for '+t.name
-
-    for t in twoDinstances:
-        del t
-
+for t in twoDinstances:
+    del t
+#########
 # If single fit
-else:
-    instance = TwoDAlphabet(inputConfigs[0],options.quicktag,options.recycleAll,stringSwaps=stringSwaps)
+# else:
+#     instance = TwoDAlphabet(inputConfigs[0],options.quicktag,options.recycleAll,stringSwaps=stringSwaps)
     
-    if not options.skipFit:
-        runMLFit([instance],options.rMin,options.rMax,systsToSet=options.setParameter,skipPlots=options.skipPlots)
-    thistag = instance.projPath
+#     if not options.skipFit:
+#         runMLFit([instance],options.rMin,options.rMax,systsToSet=options.setParameter,skipPlots=options.skipPlots)
+#     thistag = instance.projPath
 
-    # Plot
-    if not options.skipPlots:
-        try:
-            instance.plotFitResults('b')
-        except Exception as exc:
-            print traceback.format_exc()
-            print exc
-            print 'Failed to run b plots for '+instance.name
-        try:
-            instance.plotFitResults('s')
-        except Exception as exc:
-            print traceback.format_exc()
-            print exc
-            print 'Failed to run s plots for '+instance.name
+#     # Plot
+#     if not options.skipPlots:
+#         try:
+#             instance.plotFitResults('b')
+#         except Exception as exc:
+#             print traceback.format_exc()
+#             print exc
+#             print 'Failed to run b plots for '+instance.name
+#         try:
+#             instance.plotFitResults('s')
+#         except Exception as exc:
+#             print traceback.format_exc()
+#             print exc
+#             print 'Failed to run s plots for '+instance.name
 
-    del instance
+#     del instance
     
