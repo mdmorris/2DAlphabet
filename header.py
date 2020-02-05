a92c28fe73908c2d9239b5aa8e4381e13087484a
lcorcodilos <lac352@scarletmail.rutgers.edu>
2020-02-05 14:02:23 -0500

    Update FStatCalc

--- header.py
+++ header.py
@@ -419,12 +419,12 @@ def ftestInfoLookup(projInfoDict):
 
 def FStatCalc(filename1,filename2,p1,p2,n):
     print 'Calculating F statistic'
-    print 'Files: ',filename1,filename2
-    print 'Parameters: p1 %f, p2 %f, n %f'%(p1,p2,n)
     # Flip flop to make sure p2 is always greater than p1 (more parameters should always fit better)
     if p1 > p2:
         p1, p2 = p2, p1
         filename1, filename2 = filename2, filename1
+    print 'Files: ',filename1,filename2
+    print 'Parameters: p1 %f, p2 %f, n %f'%(p1,p2,n)
 
     # Get limit trees from each file
     file1 = TFile.Open(filename1)
@@ -432,21 +432,21 @@ def FStatCalc(filename1,filename2,p1,p2,n):
     file2 = TFile.Open(filename2)
     tree2 = file2.Get("limit")
     diffs=[]
-    print 'Entries ',tree1.GetEntries(),tree2.GetEntries()
+    # print 'Entries ',tree1.GetEntries(),tree2.GetEntries()
 
     # Loop over entries and calculate the F statistics
     for i in range(0,tree1.GetEntries()):
         tree1.GetEntry(i)
         tree2.GetEntry(i)
-        print 'Limits ',tree1.limit,tree2.limit
+        # print 'Limits ',tree1.limit,tree2.limit
         if tree1.limit-tree2.limit>0:
             F = (tree1.limit-tree2.limit)/(p2-p1)/(tree2.limit/(n-p2))
-            print 'Entry ',i, ":", tree2.limit, "-", tree1.limit, "=", tree2.limit-tree1.limit, "F =", F
-            diffs.append(F)
+            # print 'Entry ',i, ":", tree2.limit, "-", tree1.limit, "=", tree2.limit-tree1.limit, "F =", F
+            if F < 50: diffs.append(F)
         else:
             print 'WARNING in calculation of F statistic for entry %i. limit1-limit2 <=0 (%f - %f)' %(i,tree1.limit,tree2.limit)
 
-    print 'Diffs F stat: ',diffs
+    # print 'Diffs F stat: ',diffs
     return diffs
 
 def makeToyCard(channelNames):
@@ -1011,7 +1011,7 @@ def Condor(runscript):
         print s
         subprocess.call([s],shell=True)
 
-def StatsForCondor(run_name,projDir,toyDict,tarFilesList,commands,files_to_grab=[]):
+def StatsForCondor(run_name,toyDict,tarFilesList,commands,files_to_grab=[]):
     if len(files_to_grab) == 0:
         files_to_grab = ['higgsCombine'+run_name+'*.*.mH120.*.root','fitDiagnostics'+run_name+'*.root']
     ntoys = toyDict['ntoys']
