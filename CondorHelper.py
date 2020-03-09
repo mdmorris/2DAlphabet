b4681cc69a34c73fc4ad4af70615d89195c239cd
lcorcodilos <lac352@scarletmail.rutgers.edu>
2020-03-09 13:13:42 -0400

    Add option to specify tarball name in CondorHelper.py

--- CondorHelper.py
+++ CondorHelper.py
@@ -17,7 +17,10 @@ parser.add_option('-a', '--args', metavar='FILE', type='string', action='store',
                 default   =   '',
                 dest      =   'args',
                 help      =   'Text file with python arguments')
-
+parser.add_option('-t', '--tarball', metavar='FILE', type='string', action='store',
+                default   =   'tarball.tgz',
+                dest      =   'tarball',
+                help      =   'Name of tarball to make and send.')
 
 (options, args) = parser.parse_args()
 
@@ -25,17 +28,20 @@ commands = []
 
 print options.args
 
-if 'limits' in options.runscript and 'bstar' in options.runscript: commands.append('source condor/prep.csh')
+#if 'bstar' in options.runscript: commands.append('source condor/prep.csh')
 
 # Tar stuff
 if options.inputs != '':
-    commands.append("tar czvf tarball.tgz "+options.inputs)
+    commands.append('rm '+options.tarball)
+    commands.append("tar czvf "+options.tarball+" "+options.inputs+' --exclude="*.tgz" --exclude="*.png" --exclude="higgsCombinegof_*.root" --exclude="*.sh" --exclude="*.std*"')
 
 # Make JDL from template
 timestr = time.strftime("%Y%m%d-%H%M%S")
 out_jdl = 'temp_'+timestr+'_jdl'
-commands.append("sed 's$TEMPSCRIPT$"+options.runscript+"$g' condor/templates/jdl_template > "+out_jdl)
+commands.append("sed 's$TEMPSCRIPT$"+options.runscript+".tmp$g' condor/templates/jdl_template > "+out_jdl)
 commands.append("sed -i 's$TEMPARGS$"+options.args+"$g' "+out_jdl)
+commands.append("sed -i 's$TEMPTAR$"+options.tarball+"$g' "+out_jdl)
+commands.append("sed 's$TEMPTAR$"+options.tarball+"$g' "+options.runscript+" > "+options.runscript+'.tmp')
 commands.append("condor_submit "+out_jdl)
 commands.append("mv "+out_jdl+" condor/jdls/")
 # commands.append("condor_q lcorcodi")
--- condor/run_scripts/run_limits_bstar.sh
+++ condor/run_scripts/run_limits_bstar.sh
@@ -6,12 +6,12 @@ scramv1 project CMSSW CMSSW_10_2_13
 tar -xzf 10XwithNano.tgz
 rm 10XwithNano.tgz
 
-mkdir tardir; cp tarball.tgz tardir/; cd tardir
-tar -xzf tarball.tgz
+mkdir tardir; cp TEMPTAR tardir/; cd tardir
+tar -xzvf TEMPTAR
 cp -r * ../CMSSW_10_2_13/src/2DAlphabet/
 cd ../CMSSW_10_2_13/src/2DAlphabet/
 eval `scramv1 runtime -sh`
-scramv1 b clean; scramv1 b
+#scramv1 b clean; scramv1 b
 
 # Grab the rootfiles
 mkdir rootfiles
@@ -20,6 +20,8 @@ xrdcp root://cmseos.fnal.gov//store/user/lcorcodi/bstar_presel_rootfiles.tgz ./
 tar -xzf bstar_presel_rootfiles.tgz
 rm bstar_presel_rootfiles.tgz
 cd ../
+
+ls
 
 echo run_Limit.py $*
 python run_Limit.py $* 
