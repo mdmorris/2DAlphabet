607350e2eafc46d6f3d4f828f7b47e5ac330ae14
Lucas Corcodilos <lcorcodi@cern.ch>
2020-07-28 09:30:48 -0500

    Fix hist title so scientific notation is visible

--- header.py
+++ header.py
@@ -615,7 +615,7 @@ def makeCan(name, tag, histlist, bkglist=[],totalBkg=None,signals=[],colors=[],t
     # For example you could have:
     #   histlist = [data1, data2]
     #   bkglist = [[bkg1_1,bkg2_1],[bkg1_2,bkg2_2]]
-
+    
     if len(histlist) == 1:
         width = 800
         height = 700
@@ -647,6 +647,9 @@ def makeCan(name, tag, histlist, bkglist=[],totalBkg=None,signals=[],colors=[],t
         return 0
 
     tdrstyle.setTDRStyle()
+    gStyle.SetTitleBorderSize(0)
+    gStyle.SetTitleAlign(33)
+    gStyle.SetTitleX(.77)
 
     myCan = TCanvas(name,name,width,height)
     myCan.Divide(padx,pady)
