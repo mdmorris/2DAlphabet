9d525427a789174569f4655ca0497910d17ecc07
lcorcodilos <lac352@scarletmail.rutgers.edu>
2020-07-28 16:22:37 -0400

    Change Rpf remap from [0,1] to [-0.5,0.5] for stability

--- TwoDAlphabetClass.py
+++ TwoDAlphabetClass.py
@@ -1449,9 +1449,9 @@ class TwoDAlphabet:
                         x_center = TH2_data_fail.GetXaxis().GetBinCenter(xbin)
                         y_center = TH2_data_fail.GetYaxis().GetBinCenter(ybin)
 
-                        # Remap to [0,1]
-                        x_center_mapped = (x_center - self.newXbins['LOW'][0])/(self.newXbins['HIGH'][-1] - self.newXbins['LOW'][0])
-                        y_center_mapped = (y_center - self.newYbins[0])/(self.newYbins[-1] - self.newYbins[0])
+                        # Remap to [-0.5,0.5]
+                        x_center_mapped = (x_center - self.newXbins['LOW'][0])/(self.newXbins['HIGH'][-1] - self.newXbins['LOW'][0])-0.5
+                        y_center_mapped = (y_center - self.newYbins[0])/(self.newYbins[-1] - self.newYbins[0])-0.5
 
                         # And assign it to a RooConstVar 
                         x_const = RooConstVar("ConstVar_x_"+c+'_'+str(xbin)+'-'+str(ybin)+'_'+self.name,"ConstVar_x_"+c+'_'+str(xbin)+'-'+str(ybin)+'_'+self.name,x_center if self.rpf.fitType == 'cheb' else x_center_mapped)
@@ -1496,9 +1496,9 @@ class TwoDAlphabet:
                         x_center = TH2_data_fail.GetXaxis().GetBinCenter(xbin)
                         y_center = TH2_data_fail.GetYaxis().GetBinCenter(ybin)
 
-                        # Remap to [0,1]
-                        x_center_mapped = (x_center - self.newXbins['LOW'][0])/(self.newXbins['HIGH'][-1] - self.newXbins['LOW'][0])
-                        y_center_mapped = (y_center - self.newYbins[0])/(self.newYbins[-1] - self.newYbins[0])
+                        # Remap to [-0.5,0.5]
+                        x_center_mapped = (x_center - self.newXbins['LOW'][0])/(self.newXbins['HIGH'][-1] - self.newXbins['LOW'][0])-0.5
+                        y_center_mapped = (y_center - self.newYbins[0])/(self.newYbins[-1] - self.newYbins[0])-0.5
 
                         # Create RooConstVars to store the bin centers
                         x_const = RooConstVar("ConstVar_x_"+c+'_'+str(xbin)+'-'+str(ybin)+'_'+self.name,"ConstVar_x_"+c+'_'+str(xbin)+'-'+str(ybin)+'_'+self.name,x_center if self.rpf.fitType == 'cheb' else x_center_mapped)
@@ -2145,9 +2145,9 @@ class TwoDAlphabet:
                         thisYCenter = rpf_samples.GetYaxis().GetBinCenter(ybin)
 
                         if self.recycleAll:
-                            # Remap to [0,1]
-                            x_center_mapped = (thisXCenter - self.newXbins['LOW'][0])/(self.newXbins['HIGH'][-1] - self.newXbins['LOW'][0])
-                            y_center_mapped = (thisYCenter - self.newYbins[0])/(self.newYbins[-1] - self.newYbins[0])
+                            # Remap to [-0.5,0.5]
+                            x_center_mapped = (thisXCenter - self.newXbins['LOW'][0])/(self.newXbins['HIGH'][-1] - self.newXbins['LOW'][0])-0.5
+                            y_center_mapped = (thisYCenter - self.newYbins[0])/(self.newYbins[-1] - self.newYbins[0])-0.5
 
                             # And assign it to a RooConstVar 
                             x_const = RooConstVar("ConstVar_x_"+c+'_'+str(xbin)+'-'+str(ybin)+'_'+self.name,"ConstVar_x_"+c+'_'+str(xbin)+'-'+str(ybin)+'_'+self.name,x_center if self.rpf.fitType == 'cheb' else x_center_mapped)
