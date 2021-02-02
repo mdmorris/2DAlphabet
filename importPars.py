492d48be99693046928eaf73d383838c5cf18043
lcorcodilos <lac352@scarletmail.rutgers.edu>
2021-02-02 11:41:58 -0500

    Fix to importPars.py

--- importPars.py
+++ importPars.py
@@ -12,7 +12,7 @@ def importPars(incard,fitresults,toDrop):
     w = w_f.Get('w')
 
     for fr_name in fit_results:
-        print 'Importing '+fr_name+' ...'
+        print ('Importing '+fr_name+' ...')
         # Open fit result we want to import
         fr_f = ROOT.TFile.Open(fr_name)
         fr = fr_f.Get('fit_b') # b-only fit result (fit_s for s+b)
@@ -22,20 +22,20 @@ def importPars(incard,fitresults,toDrop):
             var = myargs.at(i)
             if var.GetName() in toDrop: continue
 
-            print var.GetName()
+            print (var.GetName())
 
             if not w.allVars().contains(var):
-                print 'WARNING: Could not find %s'%var.GetName()
+                print ('WARNING: Could not find %s'%var.GetName())
             else:
                 var_to_change = w.var(var.GetName())
-                print '\tBefore:    %.2f +/- %.2f (%.2f,%.2f)'%(var_to_change.getValV(),var_to_change.getError(),var_to_change.getMin(),var_to_change.getMax())
+                print ('\tBefore:    %.2f +/- %.2f (%.2f,%.2f)'%(var_to_change.getValV(),var_to_change.getError(),var_to_change.getMin(),var_to_change.getMax()))
                 var_to_change.setMin(var.getMin())
                 var_to_change.setMax(var.getMax())
                 var_to_change.setVal(var.getValV())
                 var_to_change.setError(var.getError())
                 
-                print '\tChange to: %.2f +/- %.2f (%.2f,%.2f)'%(var.getValV(),var.getError(),var.getMin(),var.getMax())
-                print '\tAfter:     %.2f +/- %.2f (%.2f,%.2f)'%(var_to_change.getValV(),var_to_change.getError(),var_to_change.getMin(),var_to_change.getMax())
+                print ('\tChange to: %.2f +/- %.2f (%.2f,%.2f)'%(var.getValV(),var.getError(),var.getMin(),var.getMax()))
+                print ('\tAfter:     %.2f +/- %.2f (%.2f,%.2f)'%(var_to_change.getValV(),var_to_change.getError(),var_to_change.getMin(),var_to_change.getMax()))
 
     w_f.WriteTObject(w,'w',"Overwrite")
     w_f.Close()
@@ -44,4 +44,4 @@ if __name__ == "__main__":
     incard = sys.argv[1]
     fit_results = sys.argv[2:]
     toDrop = [] # parameter names to drop from import - I've left it empty as a placeholder
-    run(incard,fitresults,toDrop)
+    importPars(incard,fit_results,toDrop)
